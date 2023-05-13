fa69d2225c3c60764e33ccd1b488840c3a2b38a2
adamtlee <adam.lee346@gmail.com>
2023-05-13 15:10:32 -0600

    Added Htx Square Lab

---
+++ src/HtxSquareLab/Program.cs
@@ -0,0 +1,15 @@
+﻿using HtxSquareLab.Services;
+
+namespace HtxSquareLab;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var squareService = new SquareService();
+
+        var result = squareService.CalculateTotalSquares(2);
+
+        Console.WriteLine(result);
+    }
+}
+
---
+++ src/HtxSquareLab/Services/SquareService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace HtxSquareLab.Services
+{
+	public class SquareService
+	{
+		public int CalculateTotalSquares(int n)
+		{
+			if (n <= 0)
+			{
+				throw new ArgumentException("N must be greater than 0");
+			}
+
+			int totalSquares = 0;
+
+            /*
+				 The number of squares in a row is equal to n - i + 1,
+				 where i is the current row number.The total count is calculated by multiplying
+				 the number of squares in the row by itself and adding it to the running total.
+			 */
+            for (int i = 1; i <= n; i++)
+			{
+				int squaresInRow = n - i + 1;
+				totalSquares += squaresInRow * squaresInRow; 
+			}
+
+			return totalSquares;
+		}
+	}
+}
+
