1dfe1dca3cbcab28f355ba7af4f892bc55d37991
adamtlee <adam.lee346@gmail.com>
2023-05-14 17:50:24 -0600

    refactored SquareLab Tests

--- src/HtxLabs.Tests/SquareLabTest.cs
+++ src/HtxLabs.Tests/SquareLabTest.cs
@@ -5,13 +5,13 @@ namespace HtxLabs.Tests
 {
 	public class SquareLabTest
 	{
-		[Fact]
-		public void TestCalculateTotalSquares_NEquals2_Returns5()
+		[Theory]
+		[InlineData(2,5)]
+		[InlineData(5, 55)]
+		public void TestCalculateTotalSquares_NEqualsPositive_ReturnsResult(int n, int expected)
 		{
 			// Arrange
 			var squareService = new SquareService();
-			int n = 2;
-			int expected = 5;
 
 			// Act
 			var sut = squareService.CalculateTotalSquares(n);
@@ -20,24 +20,10 @@ namespace HtxLabs.Tests
 			Assert.Equal(expected, sut);
 		}
 
-		[Fact]
-		public void TestCalculateTotalSquares_NEquals5_Returns55()
-		{
-            // Arrange
-            var squareService = new SquareService();
-            int n = 5;
-            int expected = 55;
-
-            // Act
-            var sut = squareService.CalculateTotalSquares(n);
-
-            // Assert
-            Assert.Equal(expected, sut);
-        }
 		[Theory]
 		[InlineData(-1)]
 		[InlineData(0)]
-		public void TestcalculateTotalSquares_NEqualsNegativeNumber_ThrowsException(int n)
+		public void TestcalculateTotalSquares_NEqualsNegativeNumberOrZero_ThrowsException(int n)
 		{
             // Arrange
             var squareService = new SquareService();
